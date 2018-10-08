059d3cc9aa6c918aa3b0586706cf0240cd21e0ad
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-08 11:17:00 -0400

    Updated to check landing page, added directory for views

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+    res.send('This will be the landing page');
+});
+
 
 app.listen(5000, function(){
     console.log('Server running on port 5000');
