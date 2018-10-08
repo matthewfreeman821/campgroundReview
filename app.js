d9a9333645403a1e6a5476900efa787c25ebd932
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-08 11:27:58 -0400

    Added routes and views for the two different routes

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send('This will be the landing page');
+    res.render('landing');
 });
+
+app.get('/campgrounds', function(req, res) {
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://pixabay.com/get/e835b20e29f7083ed1584d05fb1d4e97e07ee3d21cac104496f9c179a6e8b0b1_340.jpg"},
+        {name: "Granite Hill", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c179a6e8b0b1_340.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f9c179a6e8b0b1_340.jpg"}
+    ];
+    res.render('campgrounds');
+})
 
 
 app.listen(5000, function(){
