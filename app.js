f5fce5dc54af0d5b605490a572d017b35ec2c592
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-09 19:18:00 -0400

    Made error from seed.js go away, display comments on campground page

--- app.js
+++ app.js
@@ -54,10 +54,11 @@ app.get('/campgrounds/new', function(req, res) {
 //SHOW route - shows more info on one campground
 app.get('/campgrounds/:id', function(req, res) {
     //find the campground with provided ID
-    Campground.findById(req.params.id, function(err, foundCampground) {
+    Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground) {
         if(err) {
             console.log(err);
         } else {
+            console.log(foundCampground);
             //render show template with that campground
             res.render('show', {campground: foundCampground});
         }
