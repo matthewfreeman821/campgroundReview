191a3e635c9def83bbdba0546435e142d9b4a09b
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-09 18:23:11 -0400

    Created a models directory, updated structure

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose')
+    mongoose = require('mongoose'),
+    Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded( {extended: true}));
 app.set('view engine', 'ejs');
 
-//Schema Setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model('Campground', campgroundSchema);
-
-// Campground.create(
-//     {
-//         name: "Granite Hill",
-//         image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c179a6e8b0b1_340.jpg",
-//         description: "This is a huge granite hill...no bathroom...no water...beautiful granite!"
-//     }, function(err, campground) {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log('Newly created campground');
-//             console.log(campground);
-//         }
-//     });
 
 app.get('/', function(req, res){
     res.render('landing');
---
+++ models/campground.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+// Campground.create(
+//     {
+//         name: "Granite Hill",
+//         image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c179a6e8b0b1_340.jpg",
+//         description: "This is a huge granite hill...no bathroom...no water...beautiful granite!"
+//     }, function(err, campground) {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log('Newly created campground');
+//             console.log(campground);
+//         }
+//     });
+
+module.exports  = mongoose.model('Campground', campgroundSchema);
