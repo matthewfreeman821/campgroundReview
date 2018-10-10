c692ec5c307682a8e37607f52657e4fa38e8abf1
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-09 20:42:50 -0400

    Added sidebar to show page, styled comments

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var express = require('express'),
     Comment = require('./models/comment'),
     seedDB = require("./seeds");
 
-seedDB()
 mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded( {extended: true}));
 app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname + "/public"));
+seedDB();
 
 app.get('/', function(req, res){
     res.render('landing');
