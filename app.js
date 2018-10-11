89d008e4e1f53968b4dcb3f4664d07017d915a81
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-11 15:06:31 -0400

    Added logout route, added logic to prevent user from commenting unless signed in, added links to navbar, updated links showing

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get('/campgrounds/:id', function(req, res) {
 // COMMENTS ROUTES
 // =============================
 
-app.get ('/campgrounds/:id/comments/new', function(req, res) {
+app.get ('/campgrounds/:id/comments/new', isLoggedIn, function(req, res) {
     //find campground by id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -96,7 +96,7 @@ app.get ('/campgrounds/:id/comments/new', function(req, res) {
     });
 });
 
-app.post('/campgrounds/:id/comments', function(req, res) {
+app.post('/campgrounds/:id/comments', isLoggedIn, function(req, res) {
     //lookup campground using id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -156,6 +156,19 @@ app.post('/login', passport.authenticate('local',
         failureRedirect: '/login'
     }), function(req, res) {
 });
+
+//add logout route
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/campgrounds');
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+};
 
 app.listen(5000, function(){
     console.log('Server running on port 5000');
