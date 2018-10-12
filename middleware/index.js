b5d89de351456eb508a25f60c079a1ca9e88d0c8
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-12 12:34:09 -0400

    Refactored middleware by adding new directory and moving middleware in new directory

---
+++ middleware/index.js
@@ -0,0 +1,59 @@
+var Campground = require('../models/campground');
+var Comment = require('../models/comment');
+// All the middleware goes here
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    //if user logged in
+    if(req.isAuthenticated()) {
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if(err) {
+                res.redirect('back');
+            } else {
+                //does user own campground?
+                //foundCampground.author.id is a mongoose obj, req.user._id is a string, hence the .equals()
+                if(foundCampground.author.id.equals(req.user._id)) {
+                        next();
+                    } else {
+                        res.redirect('back');
+                }
+            }
+        });
+    } else{
+            //otherwise, redirect
+            res.redirect('back');
+    }
+    //if not, redirect
+};
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+    //if user logged in
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err) {
+                res.redirect('back');
+            } else {
+                //does user own comment?
+                //foundComment.author.id is a mongoose obj, req.user._id is a string, hence the .equals()
+                if(foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    //otherwise, redirect
+                    res.redirect('back');
+                }
+            }
+        });
+    } else{
+            //if not, redirect
+        res.redirect('back');
+    }
+};
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+};
+
+module.exports = middlewareObj;
--- routes/comments.js
+++ routes/comments.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router({mergeParams: true});//must be true in order to access campground id in params
 var Campground = require('../models/campground');
 var Comment = require('../models/comment');
+var middleware = require('../middleware');
 
 
 //Comments new
-router.get ('/new', isLoggedIn, function(req, res) {
+router.get ('/new', middleware.isLoggedIn, function(req, res) {
     //find campground by id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -17,7 +18,7 @@ router.get ('/new', isLoggedIn, function(req, res) {
 });
 
 //Comments create
-router.post('/', isLoggedIn, function(req, res) {
+router.post('/', middleware.isLoggedIn, function(req, res) {
     //lookup campground using id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -49,7 +50,7 @@ router.post('/', isLoggedIn, function(req, res) {
 });
 
 //Edit comments
-router.get('/:comment_id/edit', checkCommentOwnership, function(req, res) {
+router.get('/:comment_id/edit', middleware.checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err) {
             res.redirect('back');
@@ -60,7 +61,7 @@ router.get('/:comment_id/edit', checkCommentOwnership, function(req, res) {
 });
 
 //Update comment
-router.put('/:comment_id', checkCommentOwnership, function(req, res){
+router.put('/:comment_id', middleware.checkCommentOwnership, function(req, res){
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
         if(err) {
             res.redirect('back');
@@ -71,7 +72,7 @@ router.put('/:comment_id', checkCommentOwnership, function(req, res){
 });
 
 //Destroy comment route
-router.delete('/:comment_id', checkCommentOwnership, function(req, res){
+router.delete('/:comment_id', middleware.checkCommentOwnership, function(req, res){
     //findByIdAndRemove
     Comment.findByIdAndRemove(req.params.comment_id, function(err){
         if(err) {
@@ -82,35 +83,35 @@ router.delete('/:comment_id', checkCommentOwnership, function(req, res){
     });
 });
 
-//middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
-};
+// //middleware-no longer necessary because of refactoring
+// function isLoggedIn(req, res, next){
+//     if(req.isAuthenticated()) {
+//         return next();
+//     }
+//     res.redirect('/login');
+// };
 
-function checkCommentOwnership(req, res, next) {
-    //if user logged in
-    if(req.isAuthenticated()) {
-        Comment.findById(req.params.comment_id, function(err, foundComment) {
-            if(err) {
-                res.redirect('back');
-            } else {
-                //does user own comment?
-                //foundComment.author.id is a mongoose obj, req.user._id is a string, hence the .equals()
-                if(foundComment.author.id.equals(req.user._id)) {
-                    next();
-                } else {
-                    //otherwise, redirect
-                    res.redirect('back');
-                }
-            }
-        });
-    } else{
-        //if not, redirect
-        res.redirect('back');
-    }
-};
+// function checkCommentOwnership(req, res, next) {
+//     //if user logged in
+//     if(req.isAuthenticated()) {
+//         Comment.findById(req.params.comment_id, function(err, foundComment) {
+//             if(err) {
+//                 res.redirect('back');
+//             } else {
+//                 //does user own comment?
+//                 //foundComment.author.id is a mongoose obj, req.user._id is a string, hence the .equals()
+//                 if(foundComment.author.id.equals(req.user._id)) {
+//                     next();
+//                 } else {
+//                     //otherwise, redirect
+//                     res.redirect('back');
+//                 }
+//             }
+//         });
+//     } else{
+//         //if not, redirect
+//         res.redirect('back');
+//     }
+// };
 
 module.exports = router;
